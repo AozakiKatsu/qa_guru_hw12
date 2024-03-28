d3476052b640c272e4e5376814069cd5de726327
Irina Luchkova <shtejn@gmail.com>
2024-03-28 17:16:59 +0300

    commit

--- tests/test_add_to_favorites.py
+++ tests/test_add_to_favorites.py
@@ -4,7 +4,7 @@ from test_kazanexpress.pages.favorites_page import AddToFavorites
 def test_add_favorites():
     favorites_page = AddToFavorites()
 
-    favorites_page.open_fav()
+    favorites_page.open()
     favorites_page.add_item_to_favorites()
     favorites_page.should_item_in_favorites()
 
